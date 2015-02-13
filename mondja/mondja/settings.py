bbb7c669c9573d1708ce61b0183ff43983e2bb65
tsutsu <tsutsu0928@gmail.com>
2015-02-14 00:40:37 +0900

    staticファイル等を配置

--- mondja/mondja/settings.py
+++ mondja/mondja/settings.py
@@ -59,7 +59,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path.join(PROJECT_ROOT, 'static', 'media').replace('\\', '/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -70,7 +70,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static', 'static-only').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -78,6 +78,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    path.join(PROJECT_ROOT, 'static', 'static').replace('\\', '/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -117,6 +118,7 @@ ROOT_URLCONF = 'mondja.urls'
 WSGI_APPLICATION = 'mondja.wsgi.application'
 
 TEMPLATE_DIRS = (
+    path.join(PROJECT_ROOT, 'static', 'templates').replace('\\', '/'),
     # Put strings here, like "/home/html/django_templates" or
     # "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
