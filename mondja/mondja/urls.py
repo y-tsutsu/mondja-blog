9052aba30bbcbe97074d7a4c99f6cb3a9b4529cb
tsutsu <tsutsu0928@gmail.com>
2015-02-14 00:44:34 +0900

    管理画面を有効化．

--- mondja/mondja/urls.py
+++ mondja/mondja/urls.py
@@ -7,9 +7,9 @@ from django.conf.urls import patterns, url
 from app.forms import BootstrapAuthenticationForm
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -36,8 +36,8 @@ urlpatterns = patterns('',
         name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
