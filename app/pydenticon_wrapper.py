cafab80e6f3a177f8797968c9a7d83123a913222
tsutsu <tsutsu0928@gmail.com>
2015-03-16 23:03:27 +0900

    ブログの一覧と詳細ページにidenticonを表示するように対応．

---
+++ app/pydenticon_wrapper.py
@@ -0,0 +1,23 @@
+﻿# coding: utf-8
+
+from django.conf import settings
+import pydenticon
+import os.path
+
+dir = settings.MEDIA_ROOT.replace('/', '\\') + '\\identicon'
+
+foreground = ['rgb(45, 79, 255)', 'rgb(254, 180, 44)', 'rgb(226, 121, 234)', 'rgb(30, 179, 253)', 'rgb(232, 77, 65)', 'rgb(49, 203, 115)', 'rgb(141, 69, 170)']
+
+background = 'rgb(224, 224, 224)'
+
+padding = (20, 20, 20, 20)
+
+generator = pydenticon.Generator(5, 5, foreground = foreground, background = background)
+
+def create_identicon(username):
+    filename = dir + '\\' + username + '.png'
+    if os.path.isfile(filename): return
+
+    identicon = generator.generate(username, 200, 200, padding = padding, output_format = 'png')
+    with open(filename, 'wb') as f:
+        f.write(identicon)
--- app/views.py
+++ app/views.py
@@ -7,9 +7,10 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from django .core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from datetime import datetime
 from posts.models import Post
+from app.pydenticon_wrapper import create_identicon
 
 def top(request):
     all_posts = Post.objects.all().order_by('-pub_date')
@@ -22,6 +23,9 @@ def top(request):
         all_posts = paginator.page(1)
     except EmptyPage:
         all_posts = paginator.page(paginator.num_pages)
+
+    for item in all_posts:
+        create_identicon(item.user.username)
 
     return render(
         request,
