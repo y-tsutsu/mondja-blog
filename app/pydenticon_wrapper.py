97b23840f943d1afedf3b0c2dc54124d0edab184
tsutsu <tsutsu0928@gmail.com>
2015-03-16 23:56:21 +0900

    ファイルの区切り文字を修正．

--- app/pydenticon_wrapper.py
+++ app/pydenticon_wrapper.py
@@ -4,7 +4,7 @@ from django.conf import settings
 import pydenticon
 import os.path
 
-dir = settings.MEDIA_ROOT.replace('/', '\\') + '\\identicon'
+dir = settings.MEDIA_ROOT + '/identicon'
 
 foreground = ['rgb(45, 79, 255)', 'rgb(254, 180, 44)', 'rgb(226, 121, 234)', 'rgb(30, 179, 253)', 'rgb(232, 77, 65)', 'rgb(49, 203, 115)', 'rgb(141, 69, 170)']
 
@@ -15,7 +15,7 @@ padding = (20, 20, 20, 20)
 generator = pydenticon.Generator(5, 5, foreground = foreground, background = background)
 
 def create_identicon(username):
-    filename = dir + '\\' + username + '.png'
+    filename = dir + '/' + username + '.png'
     if os.path.isfile(filename): return
 
     identicon = generator.generate(username, 200, 200, padding = padding, output_format = 'png')
