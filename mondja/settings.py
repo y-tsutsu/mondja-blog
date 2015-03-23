657b3fd2db493952d4d63c88ea80d6a98e5a75b2
tsutsu <tsutsu0928@gmail.com>
2015-03-24 02:33:08 +0900

    markdownの変換をdjango-markdown-deuxからmarkdownに乗り換え．

--- mondja/settings.py
+++ mondja/settings.py
@@ -135,7 +135,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'gunicorn',
-    'markdown_deux',
+    'markdown',
     'posts',
     'galleries',
     'memo',
