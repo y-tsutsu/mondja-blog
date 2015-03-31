8c63f4bcba84fd2b05a4b0f86713fb1be13e7041
tsutsu <tsutsu0928@gmail.com>
2015-04-01 00:47:53 +0900

    ナビゲーションバー用のidenticon画像ファイルをMiddlewareで生成．

---
+++ mondja/middleware.py
@@ -0,0 +1,11 @@
+﻿# coding: utf-8
+
+from mondja.pydenticon_wrapper import create_identicon
+
+class MondjaMiddleware:
+
+    def process_request(self, request):
+        ''' リクエストごとにDjangoがどのビューを実行するか決定する前に呼び出されます．'''
+
+        if request.user.username != '':
+            create_identicon(request.user.username)
